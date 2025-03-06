c2fa08e9432c6d8a386b1830e920bdd1adf61c8f
Sinnosuke pro <jackhanmar00@icloud.com>
2025-03-06 15:16:23 +0900

    Create 1-05_letter.py

---
+++ 1-05_letter.py
@@ -0,0 +1,17 @@
+# Snail Mail 💌
+# Codédex
+
+print('+---------------------------------------------------------------------+')
+print('|                                                          May 3.6    |')
+print('|                                                       SAITAMA, JPN  |')
+print('|  Dear Shinosuke,                                                    |')
+print('|                                                                     |')
+print('|      Build the learn to code platform that you always dreamed of.   |')
+print('|      Give more than you take.                                       |')
+print('|      Care > capital.                                                |')
+print('|      Five-second funerals for all the Ls.                           |')
+print('|      And always get back up.                                        |')
+print('|                                                                     |')
+print('|                                              Sonny Li 🤠            |')
+print('|                                                                     |')
+print('+---------------------------------------------------------------------+')
